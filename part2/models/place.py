9e109b2a0506df88cfce6176fcb9e39a31a8f790
romain344 <romainleoulzec@gmail.com>
2025-06-19 16:41:20 +0200

    models for place

--- part2/models/place.py
+++ part2/models/place.py
@@ -1,32 +1,14 @@
-from models.base_model import BaseModel
+import re
+from basse_model import BaseMode
+from user import reviews
 
 class Place(BaseModel):
-    def __init__(self, title, description, price, latitude, longitude, owner):
+    def __init__(self, name, description, longitude, latitude, price, owner):
         super().__init__()
-
-        if not title or len(title) > 100:
-            raise ValueError("title requis et ≤ 100 caractères")
-        if price <= 0:
-            raise ValueError("Le prix doit être > 0")
-        if latitude < -90 or latitude > 90:
-            raise ValueError("Latitude invalide")
-        if longitude < -180 or longitude > 180:
-            raise ValueError("Longitude invalide")
-        if not isinstance(owner, BaseModel):
-            raise ValueError("owner doit être un utilisateur valide")
-
-        self.title = title
+        self.name = name
         self.description = description
-        self.price = price
-        self.latitude = latitude
         self.longitude = longitude
+        self.latitude = latitude
+        self.price = price
         self.owner = owner
-
-        self.reviews = []
-        self.amenities = []
-
-    def add_review(self, review):
-        self.reviews.append(review)
-
-    def add_amenity(self, amenity):
-        self.amenities.append(amenity)
+        
