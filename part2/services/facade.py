68f91c89ad5c15ee5e29d54624eda04b8b8eb083
romain344 <romainleoulzec@gmail.com>
2025-06-20 11:18:03 +0200

    facade

---
+++ part2/services/facade.py
@@ -0,0 +1,16 @@
+from app.persistence.repository import InMemoryRepository
+
+class HBnBFacade:
+    def __init__(self):
+        self.user_repo = InMemoryRepository()
+        self.place_repo = InMemoryRepository()
+        self.review_repo = InMemoryRepository()
+        self.amenity_repo = InMemoryRepository()
+
+    def create_user(self, user_data):
+        # Placeholder
+        pass
+
+    def get_place(self, place_id):
+        # Placeholder
+        pass
