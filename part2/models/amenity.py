03d64f37cf93b18508e88b71ee4286e18768e7ee
romain <romainleoulzec@gmail.com>
2025-06-22 20:31:25 +0200

    Add Amenity model with id, name, created_at, updated_at and validations

--- part2/models/amenity.py
+++ part2/models/amenity.py
@@ -1,62 +1,23 @@
-# models/amenity.py
-from basse_model import BaseModel
-from place import amenity
+from .base_model import BaseModel
 
 class Amenity(BaseModel):
-    def __init__(self, wifi, parking, garage, picine, climatisation):
+    def __init__(self, name):
         super().__init__()
-        self.wifi = wifi
-        self.parking = parking
-        self.garage = garage
-        self.picine = picine
-        self.climatisation = climatisation
+        self.name = name
     
     @property
-    # verifie qui a bien la wifie
-    def wifi(self):
-        return self.__wifi
-    @wifi.setter
-    def wifi(self, value):
-        if not isinstance(value, bool):
-            raise TypeError("doit avoir la wifie")
-        self.__wifi = value
+    def name(self):
+        return self.__name
     
-    @property
-    # verifie qui a bien le parking
-    def parking(self):
-        return self.__parking
-    @parking.setter
-    def parking(self, value):
-        if not isinstance(value, bool):
-            raise TypeError("doit avoir le parking")
-        self.__parking = value
-    
-    @property
-    # verifie qui a bien le garage
-    def garage(self):
-        return self.__garage
-    @garage.setter
-    def garage(self, value):
-        if not isinstance(value, bool):
-            raise TypeError("doit avoir le garage")
-        self.__garage = value
-    
-    @property
-    # verifie qui a bien la picine
-    def picine(self):
-        return self.__picine
-    @picine.setter
-    def picine(self, value):
-        if not isinstance(value, bool):
-            raise TypeError("doit avoir la picine")
-        self.__picine = value
-    
-    @property
-    # verifie qui a bien la climatisation
-    def climatisation(self):
-        return self.__climatisation
-    @climatisation.setter
-    def climatisation(self, value):
-        if not isinstance(value, bool):
-            raise TypeError("doit avoir la climatisation")
-        self.__climatisation = value
+    @name.setter
+    def name(self, value):
+        """
+        Définit le nom de l'amenity en vérifiant qu'il soit une chaîne non vide et de longueur max 50.
+        """
+        if not isinstance(value, str):
+            raise TypeError("name must be a string")
+        if not value:
+            raise ValueError("name cannot be empty")
+        if len(value) > 50:
+            raise ValueError("name must be 50 characters or less")
+        self.__name = value
